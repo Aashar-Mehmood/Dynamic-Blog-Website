35194ff4f19529602c20a4a2cab9f669defba9b0
Aashar Mehmood <aasharmehmood3@gmail.com>
2022-07-15 21:54:13 +0500

    made it responsive

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,8 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <header>
   <h2>Blog Website</h2>
   <nav>
+    <a id="closeMenu">&times;</a>
     <a href="index.php">Home</a>
     <?php
     if (isset($_SESSION["name"])) {
@@ -12,4 +14,17 @@
     }
     ?>
   </nav>
+  <i class="fa fa-bars" id="openMenu"></i>
 </header>
+
+<script>
+  const closeMenu = document.getElementById("closeMenu");
+  const openMenu = document.getElementById("openMenu");
+  const menu = document.querySelector("header nav");
+  openMenu.addEventListener("click", () => {
+    menu.classList.add('visible');
+  });
+  closeMenu.addEventListener("click", () => {
+    menu.classList.remove('visible');
+  });
+</script>
